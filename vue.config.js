97fba111df748199031131681c129cb86e1638db
Daniel-Venera <daniel.venera.programmer@gmail.com>
2021-06-30 14:18:08 +0200

    config deploy

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  publicPath: '/blackjack-vuejs/',
+//   css: {
+//     loaderOptions: {
+//       sass: {
+//         additionalData: `@import "@/assets/scss/variables.scss";`
+//       }
+//     }
+//   }
+}
